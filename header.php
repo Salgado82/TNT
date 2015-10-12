4e44f71f24344d313eb650dce43d70bc63173ea1
Carlos Salgado <carlosalgado.82@gmail.com>
2015-10-12 13:34:36 -0500

    El primero

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>TNT</title>
+
+	<!-- Archivos CSS necesarios -->
+	<link type="text/css" rel="stylesheet" href="utilidades/boot/css/bootstrap.min.css">
+
+	<!-- Archivos Js necesarios -->
+	<script src="utilidades/jquery/jquery-1.11.3.min.js"></script>
+	<script src="utilidades/boot/js/bootstrap.min.js"></script>
+
+</head>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!-- Incluímos el header -->
+<?php include_once 'header.php';?>
+	
+	<!-- ARCHIVOS PROPIOS DEL CSS Y JS -->
+	<link rel="stylesheet" href="css/top.css">
+
+</head>
+
+<body>
+<div id="wrap">
+
+	<div id="container">
+	
+		<!-- Top con menu -->
+		<section id="top">
+			<div id="row">
+				<div id="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					
+					<nav class="navbar navbar-default navbar-fixed-top">
+      					<div class="container">
+       	 					<div class="navbar-header">
+          						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            					<span class="sr-only">Toggle navigation</span>
+						            <span class="icon-bar"></span>
+						            <span class="icon-bar"></span>
+						            <span class="icon-bar"></span>
+          						</button>
+          						<a class="navbar-brand" href="#"><img src="img/logo.png" alt="Logo TNT" width="80px"></a>
+        					</div>
+        					<div id="navbar" class="navbar-collapse collapse">
+          						<ul class="nav navbar-nav navbar-right">
+						            <li class="active"><a href="#">Home</a></li>
+						            <li><a href="#">Program</a></li>
+						            <li><a href="#">Participants</a></li>
+						            <li><a href="#">News</a></li>
+						            <li><a href="#">Sponsors</a></li>
+          						</ul>
+        					</div>
+      					</div>
+    				</nav>
+
+
+				</div>
+			</div>
+		</section>
+		<!-- /Top con menu -->
+
+	</div>
+</div>
+
+</body>
+</html>
