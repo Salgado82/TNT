2449c55240bb6d9f42d0918a96418effffd3d217
Salgado82 <carlosalgado.82@gmail.com>
2015-10-13 16:37:57 -0500

    Orden perfecto

--- index.php
+++ index.php
@@ -1,139 +1,124 @@
-<!-- Incluímos el header -->
-<?php include_once 'header.php';?>
-	
-	<!-- ARCHIVOS PROPIOS DEL CSS Y JS -->
-	<link rel="stylesheet" href="css/top.css">
-	<link  rel="stylesheet" href="css/carousel2.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<script src="utilidades/scroll/jquery.easing.min.js"></script>
-	<script src="js/scroll_menu.js"></script>
+    <title>TNT</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="utilidades/boot/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Style CSS -->
+    <link href="css/style.css" rel="stylesheet">
 
 </head>
 
-<body>
-<div id="wrap">
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
-	<div id="container">
-	
-		<!-- Top con menu -->
-		<section id="top">
-			<div id="row">
-				<div id="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-					
-					<nav class="navbar navbar-default navbar-fixed-top">
-      					<div class="container">
-       	 					<div class="navbar-header">
-          						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	            					<span class="sr-only">Toggle navigation</span>
-						            <span class="icon-bar"></span>
-						            <span class="icon-bar"></span>
-						            <span class="icon-bar"></span>
-          						</button>
-          						<a class="navbar-brand" href="#"><img src="img/logo.png" alt="Logo TNT" width="80px"></a>
-        					</div>
-        					<div id="navbar" class="navbar-collapse collapse">
-          						<ul class="nav navbar-nav navbar-right">
-						            <li class="active"><a href="#">Home</a></li>
-						            <li><a href="#">Program</a></li>
-						            <li><a href="#">Participants</a></li>
-						            <li><a href="#">News</a></li>
-						            <li><a href="#">Sponsors</a></li>
-          						</ul>
-        					</div>
-      					</div>
-    				</nav>
-
-
-				</div>
-			</div>
-		</section>
-		<!-- /Top con menu -->
-
-		<form class="navbar-form navbar-left" role="search">
-		  <div class="form-group">
-		    <p class="navbar-text">NEWS</p>
-		  </div>
-		</form>
-
-		<!-- /Carousel -->
-		    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-     <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active">
-          <div class="row">
-            <div class="col-sm-6 col-md-6">
-              <div class="thumbnail">
-                <img src="http://placehold.it/545x343" width="480px">
-                <div class="col-md-offset-1 col-md-10">
-                <h3>HOLA</h3>
-                  <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
-              </div>
-              </div>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top"><img src="img/logo.png" alt="Logo TNT" width="80px"></a>
             </div>
 
-            <div class="col-sm-6 col-md-6">
-              <div class="thumbnail">
-                <img src="http://placehold.it/545x343">
-                <h3></h3>
-                <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
-              </div>
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#top">Home</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#program">Program</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#participants">Participants</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#news">News</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#sponsors">Sponsors</a>
+                    </li>
+                </ul>
             </div>
-          </div>
         </div>
+    </nav>
 
-        <div class="item">
-          <div class="row">
-            <div class="col-sm-6 col-md-6">
-              <div class="thumbnail">
-                <img src="http://placehold.it/545x343">
-                <h3></h3>
-                  <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
-              </div>
+    <!-- Home Section -->
+    <section id="top" class="top-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Home Section</h1>
+                </div>
             </div>
-
-            <div class="col-sm-6 col-md-6">
-              <div class="thumbnail">
-                <img src="http://placehold.it/545x343">
-                <h3></h3>
-                <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
-              </div>
-            </div>
-          </div>
         </div>
+    </section>
 
-        <div class="item">
-          <div class="row">
-            <div class="col-sm-6 col-md-6">
-              <div class="thumbnail">
-                <img src="http://placehold.it/545x343">
-                <h3></h3>
-                  <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
-              </div>
+    <!-- News Section -->
+    <section id="news" class="news-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>News Section</h1>
+                </div>
             </div>
-
-            <div class="col-sm-6 col-md-6">
-              <div class="thumbnail">
-                <img src="http://placehold.it/545x343">
-                <h3></h3>
-                <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
+    </section>
 
-      <!-- Controls -->
-      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
-    </div>
-		<!-- /Carousel -->
+    <!-- Sponsors Section -->
+    <section id="sponsors" class="sponsors-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Sponsors Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
 
-	</div>
-</div>
+    <!-- Program Section 
+    <section id="program" class="program-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Program Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    -->
+
+    <!-- Participants Section -->
+    <section id="participants" class="participants-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Participants Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- jQuery -->
+    <script src="utilidades/jquery/jquery.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="utilidades/boot/js/bootstrap.min.js"></script>
+
+    <!-- Scrolling Js -->
+    <script src="utilidades/boot/js/jquery.easing.min.js"></script>
+    <script src="js/menu.js"></script>
 
 </body>
+
 </html>
